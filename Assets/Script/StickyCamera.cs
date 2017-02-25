47b97b8b384392436a2daca82f284eefff5ae829
Mathieu Auclair <mathiass12@hotmail.com>
2017-02-24 20:11:22 -0500

    adding a sticky camera

--- Assets/Script/StickyCamera.cs
+++ Assets/Script/StickyCamera.cs
@@ -3,13 +3,20 @@ using System.Collections;
 
 public class StickyCamera : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+	public GameObject playerFollowed;
+	private float moveX, moveY;
+
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void LateUpdate () {
+		moveX = 0;
+		moveY = 0;
+		if (playerFollowed.transform.position.x != this.transform.position.x) {
+			moveX = playerFollowed.transform.position.x - this.transform.position.x;
+		}
+		if (playerFollowed.transform.position.y != this.transform.position.y) {
+			moveY = playerFollowed.transform.position.y - this.transform.position.y;
+		}
+		this.transform.Translate (moveX*Time.deltaTime, moveY*Time.deltaTime, 0);
 	}
 }
