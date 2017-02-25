82b1573156faf01845bf0b8cc257c397305038cf
Mathieu Auclair <mathiass12@hotmail.com>
2017-02-25 12:06:04 -0500

    add button door

---
+++ Assets/Script/doorHelpLabel.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class doorHelpLabel : MonoBehaviour {
+
+	public Text openDoor;
+
+	void OntriggerEnter2D (Collider2D other) {
+		openDoor.text = "Press button to open";
+	}
+
+	void OntriggerExit2D (Collider2D other) {
+		openDoor.text = "";
+	}
+}
