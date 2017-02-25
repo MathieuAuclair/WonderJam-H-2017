8acbbb7bea89e8a4e236e37b69cee3881885c349
Mathieu Auclair <mathiass12@hotmail.com>
2017-02-25 16:01:52 -0500

    adding a control to acces ui

---
+++ Assets/Script/DisplayScore.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisplayScore : MonoBehaviour {
+
+	private bool UIshown = true;
+	public GameObject player;
+	public GameObject camScene;
+	public static int stamina = 0, propaganda = 0, suspicius = 0; 
+
+	private float time = 0;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxis ("Fire2") != 0 && UIshown && time > 2) {
+			UIshown = false;
+			time = 0;
+			camScene.GetComponent<StickyCamera> ().playerFollowed = this.gameObject;
+		} 
+		else if (Input.GetAxis ("Fire2") != 0 && time > 2) {
+			camScene.GetComponent<StickyCamera> ().playerFollowed = player;
+			time = 0;
+			UIshown = true;
+		}
+		if(time <= 2)
+		time += Time.deltaTime;
+	}
+}
